8a414b5f1d4d11a958d675f3f528009029d86969
Fahim Shahriyer <fahimshahriyer@outlook.com>
2016-01-15 19:10:51 +0600

    Added mdl

---
+++ database/migrations/2016_01_06_202816_create_sources_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sources', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->string('name',128);
+            $table->string('url',256);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sources');
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,36 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/css/app.css">
 </head>
-<body>
+<body class="wrapper-home">
+    <div id="left-sidebar">
+        <div class="uk-flex uk-flex-center uk-flex-space-between uk-flex-column uk-height-1-1">
+            <div>
+                <h1 class="text-white text-thin"><i class="uk-icon-newspaper-o"></i> <span class="uk-text-bold">R</span>EWS</h1>
+            </div>
+            <div>
+                <ul id="right-sidebar-nav">
+                    <li><a href="#"><i class="uk-icon-bolt"></i> Recent</a></li>
+                    <li><a href="#"><i class="uk-icon-fire"></i> Rocemmended</a></li>
+                    <li><a href="#"><i class="uk-icon-thumb-tack"></i> Pinned</a></li>
+                    <li><a href="#"><i class="uk-icon-heartbeat"></i> Favourites</a></li>
+                    <li><a href="#"><i class="uk-icon-user-secret"></i> People</a></li>
+                </ul>
+            </div>
+            <div>
+                <a href="#">
+                    <div class="uk-flex uk-flex-middle uk-flex-space-between">
+                        <div>   
+                            <img src="{{ url('thumbs/avatar.png')}}" alt="avatar" class="uk-border-circle" width="42px">
+                        </div>
+                        <div>Fahim Shahriyer</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div id="right-sidebar">
+        
+    </div>
     <nav class="uk-navbar uk-navbar-attached">
         <div class="uk-container uk-container-center">
             <a href="{{ url('/home')}}" class="uk-navbar-brand">REWS</a>
