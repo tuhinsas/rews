88d5780cdcec0eab3c83d7d3098af3205d819bca
Fahim Shahriyer <fahimshahriyer@outlook.com>
2016-01-27 00:51:19 +0600

    Updated Source Migration

--- database/migrations/2016_01_06_202816_create_sources_table.php
+++ database/migrations/2016_01_06_202816_create_sources_table.php
@@ -17,11 +17,8 @@ class CreateSourcesTable extends Migration
             $table->string('name',128);
             $table->string('type',64);
             $table->string('url',256);
-            $table->integer('category_id')->unsigned();
             $table->boolean('autograb')->default(true);
-            $table->time('start')->default('12:00');
-            $table->time('duration')->default('06:00');
-            $table->boolean('active')->default(true);
+            $table->enum('status',['published','hidden'])->default('published');
             $table->timestamps();
         });
     }
