ed7f42298456d69998ddb5e6f0c4663d82994701
Fahim Shahriyer <fahimshahriyer@outlook.com>
2016-01-27 01:26:21 +0600

    Little tweak in source index view

--- resources/views/admin/source/index.blade.php
+++ resources/views/admin/source/index.blade.php
@@ -2,34 +2,38 @@
 @section('title','Create New Source')
 @section('content')
 	<h1>All Sources</h1>
-	<table class="uk-table">
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th>Type</th>
-				<th>Autograb</th>
-				<th>Status</th>
-				<th>Last Updated</th>
-				<th>Action</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach($sources as $source)
-			<tr>
-				<td>{{ $source->name }}</td>
-				<td>{{ $source->type }}</td>
-				<td>{{ $source->autograb }}</td>
-				<td>{{ $source->status}}</td>
-				<td>{{ $source->updated_at->diffForHumans() }}</td>
-				<td>
-					<div class="uk-button-group">
-						<a href="{{ route('admin.source.edit',$source->id) }}" class="uk-button uk-button-primary"><i class="uk-icon-edit"></i> Edit</a>
-						<a href="{{ route('admin.source.confirm',$source->id) }}" class="uk-button uk-button-danger"><i class="uk-icon-trash"></i> Delete</a>
-					</div>
-				</td>
-			</tr>
-			@endforeach
-		</tbody>
-	</table>
+	<div class="uk-overflow-container">
+		<table class="uk-table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Type</th>
+					<th>URL</th>
+					<th>Autograb</th>
+					<th>Status</th>
+					<th>Last Updated</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($sources as $source)
+				<tr>
+					<td>{{ $source->name }}</td>
+					<td>{{ $source->type }}</td>
+					<td class="uk-text-primary uk-text-small uk-text-bold">{{ $source->url }}</td>
+					<td>{{ $source->autograb }}</td>
+					<td>{{ $source->status}}</td>
+					<td>{{ $source->updated_at->diffForHumans() }}</td>
+					<td>
+						<div class="uk-button-group">
+							<a href="{{ route('admin.source.edit',$source->id) }}" class="uk-button uk-button-primary"><i class="uk-icon-edit"></i> Edit</a>
+							<a href="{{ route('admin.source.confirm',$source->id) }}" class="uk-button uk-button-danger"><i class="uk-icon-trash"></i> Delete</a>
+						</div>
+					</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
 	{!! $sources->render() !!}
 @endsection
